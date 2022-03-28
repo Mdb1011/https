429b7fec0296dbaf79520e9190e7c96115711706
Mohammad Tomor <89591649+Mdb1011@users.noreply.github.com>
2022-03-28 19:02:07 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,87 @@
+from time import strftime
+import os
+from time import sleep
+from random import choice
+from rubika.client import Bot
+bot = Bot("AppName", auth="kfiasdspeuoncidrixxesokhedxjkbyl")
+ls = "u0C6k8H00b77b4607c2e2e7d964fe5aa"
+with open("a.txt") as f:
+	j=f.read().splitlines()
+while True:
+	time = strftime("%H%M%S")
+	print(time)
+	if time =="010000":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="020000":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="020000":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="030000":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="040000":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="050000":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="060000":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="070000":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="080000":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="090000":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="101000":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="111100":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="121200":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="131300":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="141400":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="151500":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="161600":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="171700":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="181800":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="191900":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="202000":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="212100":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="222200":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="232300":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
+	elif time =="000000":
+		sleep(1)
+		bot.sendMessage(ls, choice(j))
